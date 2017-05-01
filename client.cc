0f247d113c6806804f441ab9c350d2676732265e
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-05-01 17:56:19 +0200

    Reformat code, move err.c to private

--- client.cc
+++ client.cc
@@ -10,79 +10,87 @@
 
 #define BUFFER_SIZE 1000
 
+void send_datagram() {
+
+}
+
+void receive_datagram() {
+
+}
+
+
 int main(int argc, char *argv[]) {
-  int sock;
-  struct addrinfo addr_hints;
-  struct addrinfo *addr_result;
+    int sock;
+    struct addrinfo address_hints;
+    struct addrinfo *address_result;
 
-  int i, flags, sflags;
-  char buffer[BUFFER_SIZE];
-  size_t len;
-  ssize_t snd_len, rcv_len;
-  struct sockaddr_in my_address;
-  struct sockaddr_in srvr_address;
-  socklen_t rcva_len;
+    int i, flags, sflags;
+    char buffer[BUFFER_SIZE];
+    size_t length;
+    ssize_t send_length, receive_length;
+    struct sockaddr_in my_address;
+    struct sockaddr_in server_address;
+    socklen_t receive_address_length;
 
-  if (argc < 3) {
-    fatal("Usage: %s host port message ...\n", argv[0]);
-  }
-
-  // 'converting' host/port in string to struct addrinfo
-  (void) memset(&addr_hints, 0, sizeof(struct addrinfo));
-  addr_hints.ai_family = AF_INET; // IPv4
-  addr_hints.ai_socktype = SOCK_DGRAM;
-  addr_hints.ai_protocol = IPPROTO_UDP;
-  addr_hints.ai_flags = 0;
-  addr_hints.ai_addrlen = 0;
-  addr_hints.ai_addr = NULL;
-  addr_hints.ai_canonname = NULL;
-  addr_hints.ai_next = NULL;
-  if (getaddrinfo(argv[1], NULL, &addr_hints, &addr_result) != 0) {
-    syserr("getaddrinfo");
-  }
-
-  my_address.sin_family = AF_INET; // IPv4
-  my_address.sin_addr.s_addr =
-      ((struct sockaddr_in*) (addr_result->ai_addr))->sin_addr.s_addr; // address IP
-  my_address.sin_port = htons((uint16_t) atoi(argv[2])); // port from the command line
-
-  freeaddrinfo(addr_result);
-
-  sock = socket(PF_INET, SOCK_DGRAM, 0);
-  if (sock < 0)
-    syserr("socket");
-
-  for (i = 3; i < argc; i++) {
-    len = strnlen(argv[i], BUFFER_SIZE);
-    if (len == BUFFER_SIZE) {
-      (void) fprintf(stderr, "ignoring long parameter %d\n", i);
-      continue;
-    }
-    (void) printf("sending to socket: %s\n", argv[i]);
-    sflags = 0;
-    rcva_len = (socklen_t) sizeof(my_address);
-    snd_len = sendto(sock, argv[i], len, sflags,
-        (struct sockaddr *) &my_address, rcva_len);
-    if (snd_len != (ssize_t) len) {
-      syserr("partial / failed write");
+    if (argc < 3 || argc > 4) {
+        printf("Usage: %s timestamp c host [port]\n", argv[0]);
+        return 1;
     }
 
-    (void) memset(buffer, 0, sizeof(buffer));
-    flags = 0;
-    len = (size_t) sizeof(buffer) - 1;
-    rcva_len = (socklen_t) sizeof(srvr_address);
-    rcv_len = recvfrom(sock, buffer, len, flags,
-        (struct sockaddr *) &srvr_address, &rcva_len);
-
-    if (rcv_len < 0) {
-      syserr("read");
+    memset(&address_hints, 0, sizeof(struct addrinfo));
+    address_hints.ai_family = AF_INET; // IPv4
+    address_hints.ai_socktype = SOCK_DGRAM;
+    address_hints.ai_protocol = IPPROTO_UDP;
+    address_hints.ai_flags = 0;
+    address_hints.ai_addrlen = 0;
+    address_hints.ai_addr = NULL;
+    address_hints.ai_canonname = NULL;
+    address_hints.ai_next = NULL;
+    if (getaddrinfo(argv[1], NULL, &address_hints, &address_result) != 0) {
+        syserr("getaddrinfo");
     }
-    (void) printf("read from socket: %zd bytes: %s\n", rcv_len, buffer);
-  }
 
-  if (close(sock) == -1) { //very rare errors can occur here, but then
-    syserr("close"); //it's healthy to do the check
-  }
+    my_address.sin_family = AF_INET; // IPv4
+    my_address.sin_addr.s_addr = ((struct sockaddr_in*)address_result->ai_addr)->sin_addr.s_addr;
+    my_address.sin_port = htons((uint16_t)atoi(argv[2]));
 
-  return 0;
+    freeaddrinfo(address_result);
+
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        syserr("socket");
+    }
+
+    for (i = 3; i < argc; i++) {
+        length = strnlen(argv[i], BUFFER_SIZE);
+        if (length == BUFFER_SIZE) {
+            fprintf(stderr, "ignoring long parameter %d\n", i);
+            continue;
+        }
+
+        printf("sending to socket: %s\n", argv[i]);
+        sflags = 0;
+        receive_address_length = (socklen_t)sizeof(my_address);
+        send_length = sendto(sock, argv[i], length, sflags, (struct sockaddr *)&my_address, receive_address_length);
+        if (send_length != (ssize_t)length) {
+            syserr("partial / failed write");
+        }
+
+        (void) memset(buffer, 0, sizeof(buffer));
+        flags = 0;
+        length = (size_t)sizeof(buffer) - 1;
+        receive_address_length = (socklen_t)sizeof(server_address);
+        receive_length = recvfrom(sock, buffer, length, flags, (struct sockaddr *)&server_address, &receive_address_length);
+
+        if (receive_length < 0) {
+            syserr("read");
+        }
+        printf("read from socket: %zd bytes: %s\n", receive_length, buffer);
+    }
+
+    if (close(sock) == -1) {
+        syserr("close");
+    }
+
+    return 0;
 }
--- server.cc
+++ server.cc
@@ -11,51 +11,46 @@
 #define PORT_NUM     10001
 
 int main(int argc, char *argv[]) {
-	int sock;
-	int flags, sflags;
-	struct sockaddr_in server_address;
-	struct sockaddr_in client_address;
+    int sock;
+    int flags, sflags;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
 
-	char buffer[BUFFER_SIZE];
-	socklen_t snda_len, rcva_len;
-	ssize_t len, snd_len;
+    char buffer[BUFFER_SIZE];
+    socklen_t sender_address_length, receiver_address_length;
+    ssize_t len, snd_len;
 
-	sock = socket(AF_INET, SOCK_DGRAM, 0); // creating IPv4 UDP socket
-	if (sock < 0)
-		syserr("socket");
-	// after socket() call; we should close(sock) on any execution path;
-	// since all execution paths exit immediately, sock would be closed when program terminates
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0)
+        syserr("socket");
 
-	server_address.sin_family = AF_INET; // IPv4
-	server_address.sin_addr.s_addr = htonl(INADDR_ANY); // listening on all interfaces
-	server_address.sin_port = htons(PORT_NUM); // default port for receiving is PORT_NUM
+    server_address.sin_family = AF_INET; // IPv4
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY); // listening on all interfaces
+    server_address.sin_port = htons(PORT_NUM); // default port for receiving is PORT_NUM
 
-	// bind the socket to a concrete address
-	if (bind(sock, (struct sockaddr *) &server_address,
-			(socklen_t) sizeof(server_address)) < 0)
-		syserr("bind");
+    if (bind(sock, (struct sockaddr *)&server_address, (socklen_t)sizeof(server_address)) < 0)
+        syserr("bind");
 
-	snda_len = (socklen_t) sizeof(client_address);
-	for (;;) {
-		do {
-			rcva_len = (socklen_t) sizeof(client_address);
-			flags = 0; // we do not request anything special
-			len = recvfrom(sock, buffer, sizeof(buffer), flags,
-					(struct sockaddr *) &client_address, &rcva_len);
-			if (len < 0)
-				syserr("error on datagram from client socket");
-			else {
-				(void) printf("read from socket: %zd bytes: %.*s\n", len,
-						(int) len, buffer);
-				sflags = 0;
-				snd_len = sendto(sock, buffer, (size_t) len, sflags,
-						(struct sockaddr *) &client_address, snda_len);
-				if (snd_len != len)
-					syserr("error on sending datagram to client socket");
-			}
-		} while (len > 0);
-		(void) printf("finished exchange\n");
-	}
+    sender_address_length = (socklen_t)sizeof(client_address);
+    for (;;) {
+        do {
 
-	return 0;
+            receiver_address_length = (socklen_t)sizeof(client_address);
+            flags = 0;
+
+            len = recvfrom(sock, buffer, sizeof(buffer), flags, (struct sockaddr *)&client_address, &receiver_address_length);
+            if (len < 0) {
+                syserr("error on datagram from client socket");
+            } else {
+                printf("read from socket: %zd bytes: %.*s\n", len, (int) len, buffer);
+                sflags = 0;
+                snd_len = sendto(sock, buffer, (size_t) len, sflags, (struct sockaddr *)&client_address, sender_address_length);
+                if (snd_len != len) {
+                    syserr("error on sending datagram to client socket");
+                }
+            }
+        } while (len > 0);
+
+        printf("finished exchange\n");
+    }
 }
